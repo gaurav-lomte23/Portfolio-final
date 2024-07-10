1ccaf38857d3ebf5d8627090ec4fe572e07d3334
Gaurav23 <97234987+gaurav-lomte23@users.noreply.github.com>
2024-07-10 23:19:23 +0530

    Update Heros.jsx
    Signed-off-by: Gaurav23 <97234987+gaurav-lomte23@users.noreply.github.com>

--- src/sections/Hero/Heros.jsx
+++ src/sections/Hero/Heros.jsx
@@ -1,5 +1,5 @@
 import styles from './HeroStyles.module.css';
-import Hero from '../../assets/Heroani.png';
+// import Hero from '../../assets/Heroani.png';
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
 import twitterLight from '../../assets/twitter-light.svg';
@@ -10,7 +10,8 @@ import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
 import { useThemeContext } from '../../common/ThemeContext';
  import Gaurav from '../../assets/GauravL-Resume.pdf';
- 
+ import Hero2 from '../../assets/Hero2.png';
+
 function Heros() {
   const { theme, toggleTheme } = useThemeContext();
 
@@ -23,7 +24,7 @@ function Heros() {
     <section id="hero" className={styles.container}>
       <div className={styles.colorModeContainer}>
         <img
-          src={Hero}
+          src={Hero2}
           className={styles.hero}
           alt="Profile picture of Gaurav Lomte"
         />
@@ -33,28 +34,31 @@ function Heros() {
           alt="Color mode icon"
           onClick={toggleTheme}
         />
+        <p className={styles.colorMode2}><b>↑click </b></p>
       </div>
       <div className={styles.info}>
         <h1 className={styles.name}><b>
-         Gaurav Lomte</b>
+         Gaurav    Lomte</b>
         </h1>
-        <h2>Frontend Developer</h2>
+        <h2><i>Frontend Developer</i></h2>
         <span>
-          <a href="https://twitter.com/" target="blank">
+          <a href="https://www.hackerrank.com/profile/gauravlomte23" target="blank">
             <img src={twitterIcon} alt="Twitter icon" />
           </a>
-          <a href="https://github.com/" target="_blank">
+          <a href="https://github.com/gaurav-lomte23" target="_blank">
             <img src={githubIcon} alt="Github icon" />
           </a>
-          <a href="https://linkedin.com/" target="_blank">
+          <a href="https://www.linkedin.com/in/gaurav-lomte-ab881321a/" target="_blank">
             <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
         </span>
         <p className={styles.description}>
-        Hello! I’m Gaurav, a passionate Developer with expertise in designing, developing, and deploying high-quality web applications. My journey in the tech world began with a fascination for solving complex problems.
+        Hello! I’m Gaurav, a passionate Developer with expertise in designing, developing, and deploying high-quality web applications.<br/><br/> My journey in the tech world began with a fascination for solving complex problems.
         </p>
         < a href={Gaurav} download="GauravL-Resume.pdf">
-          <button className="hover">Resume</button>
+          <button className="hover">Resume<br/>
+          </button>
+          download
         </a>
       </div>
     </section>
